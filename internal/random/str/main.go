de156d4d34a030b66819132cd53dcf6992c4fedd
Real <swrely@qq.com>
2024-12-13 01:10:55 +0800

    feat[字符串]: 28. 找出字符串中第一个匹配项的下标

--- internal/random/str/main.go
+++ internal/random/str/main.go
@@ -121,3 +121,115 @@ func reverseWords(s string) string {
 
 	return strings.Join(words, " ")
 }
+
+// 28. 找出字符串中第一个匹配项的下标
+// https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return -1
+	}
+
+	// 使用 KMP 算法实现
+	next := calcNext(needle)
+	// 模式串起始的下标位置
+	j := 0
+	for i := 0; i < len(haystack); i++ {
+		for j > 0 && haystack[i] != needle[j] {
+			// 回退到 next 数组的前一位
+			j = next[j-1]
+		}
+		// 每次前移的只有模式串
+		if haystack[i] == needle[j] {
+			j++
+		}
+		if j == len(needle) {
+			return i - len(needle) + 1
+		}
+	}
+
+	return -1
+}
+
+func calcNext(s string) []int {
+	next := make([]int, len(s))
+	// i 为后缀，j 为前缀
+	for i := 1; i < len(s); i++ {
+		prefix := []byte(s[:i])
+		suffix := []byte(s[1 : i+1])
+		next[i] = compare(prefix, suffix)
+	}
+
+	return next
+}
+
+func compare(a, b []byte) int {
+	// 比对前后缀是否相等，算出相等先后缀的长度
+	for len(a) > 0 && len(b) > 0 {
+		sameCnt := 0
+		for i := 0; i < len(a); i++ {
+			if a[i] != b[i] {
+				// 不相等则再取各自的前后缀
+				a = a[:len(a)-1]
+				b = b[1:]
+				sameCnt = 0
+				break
+			}
+
+			sameCnt++
+		}
+
+		if sameCnt == len(a) {
+			return len(a)
+		}
+	}
+
+	return 0
+}
+
+// 28. 找出字符串中第一个匹配项的下标
+// https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string
+func strStrWithNext(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return -1
+	}
+
+	// 使用 KMP 算法实现
+	next := calcNextArr(needle)
+	// 模式串起始的下标位置
+	j := 0
+	for i := 0; i < len(haystack); i++ {
+		for j > 0 && haystack[i] != needle[j] {
+			// 回退到 next 数组的前一位
+			j = next[j-1]
+		}
+		// 每次前移的只有模式串
+		if haystack[i] == needle[j] {
+			j++
+		}
+		if j == len(needle) {
+			return i - len(needle) + 1
+		}
+	}
+
+	return -1
+}
+
+func calcNextArr(needle string) []int {
+	next := make([]int, len(needle))
+	j := 0
+	for i := 1; i < len(next); i++ {
+		for j > 0 && needle[i] != needle[j] {
+			// 回退到前一位
+			j = next[j-1]
+		}
+
+		if needle[i] == needle[j] {
+			j++
+		}
+
+		// 当前最长相等前后缀长度
+		next[i] = j
+	}
+
+	return next
+}
